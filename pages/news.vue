3ae1710433a81830ed218a22e2659dcd101f8dc2
adwos <makar.savchuk.2020@gmail.com>
2024-06-10 19:10:01 +0300

    add update

--- pages/news.vue
+++ pages/news.vue
@@ -3,7 +3,62 @@
 </script>
 
 <template>
-  <h1>news</h1>
+
+
+
+
+
+  <div class="grid grid-cols-5 grid-rows-5 gap-4">
+    <div class="col-start-2">
+      <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+        <img class="w-full h-auto rounded-t-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80" alt="Image Description">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            Card title
+          </h3>
+          <p class="mt-1 text-gray-500 dark:text-neutral-400">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <p class="mt-5 text-xs text-gray-500 dark:text-neutral-500">
+            Last updated 5 mins ago
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-start-3">
+      <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+        <img class="w-full h-auto rounded-t-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80" alt="Image Description">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            Card title
+          </h3>
+          <p class="mt-1 text-gray-500 dark:text-neutral-400">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <p class="mt-5 text-xs text-gray-500 dark:text-neutral-500">
+            Last updated 5 mins ago
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-start-4">
+      <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+        <img class="w-full h-auto rounded-t-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80" alt="Image Description">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            Card title
+          </h3>
+          <p class="mt-1 text-gray-500 dark:text-neutral-400">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <p class="mt-5 text-xs text-gray-500 dark:text-neutral-500">
+            Last updated 5 mins ago
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
