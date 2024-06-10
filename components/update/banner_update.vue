7cee0ed23890c5228f332796d20e515565260d5a
adwos <makar.savchuk.2020@gmail.com>
2024-06-10 18:56:30 +0300

    add update

---
+++ components/update/banner_update.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="flex flex-col justify-center pb-4 w-full bg-white shadow max-md:max-w-full border-t">
+
+    <div class="flex gap-5 justify-between self-center mt-4 max-w-full w-[1201px] max-md:flex-wrap">
+      <div class="flex flex-col px-5 my-auto">
+        <h2 class="text-xl text-black">Обновления Adwos</h2>
+        <div class="flex gap-3 px-px mt-3.5 text-xs text-black text-opacity-70">
+          <a href="#" class="justify-center whitespace-nowrap">Публичные</a>
+          <a href="#" class="justify-center whitespace-nowrap">Бета-версии</a>
+          <a href="#" class="justify-center">Механизм работы ↗</a>
+        </div>
+      </div>
+      <div class="flex gap-3 max-md:flex-wrap">
+        <div class="flex gap-2 px-3.5 py-2.5 text-white bg-blue-500 rounded-lg max-sm:ml-2.5">
+          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/090ad1622b5590249cff8594c88064b1b47e687f5adc57ce7aed63be4d9ae20a?apiKey=f092707241b642e585b4eb40b45858e9&" alt="" class="shrink-0 self-start w-5 aspect-[0.83]" />
+          <div class="flex flex-col">
+            <h3 class="text-sm">Публичная версия</h3>
+            <div class="flex gap-1 text-xs">
+              <span>3.0</span>
+              <span>выпущена</span>
+              <time datetime="2024-04-29">29 апреля 2024</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>О компании</h1>
+</template>
+
+<style scoped>
+
+</style>
