48c1814084ef1996a93773a5ff2106eaea925fc7
adwos <makar.savchuk.2020@gmail.com>
2024-06-12 18:28:32 +0300

    add update

---
+++ components/nations.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="container mx-auto pt-8 px-4">
+    <div class="md:flex justify-between">
+      <div class="flex items-center justify-center">
+        <div class="flex md:flex-col md:mr-8 items-center">
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 md:mb-12 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="63-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇸🇦"
+              data-translation="🇸🇦" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇸🇦</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="63-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇮🇳"
+              data-translation="🇮🇳" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇮🇳</ya-tr-span></span></button>
+        </div>
+        <div class="flex md:flex-col items-center">
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 md:mb-12 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="64-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇺🇸"
+              data-translation="🇺🇸" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇺🇸</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 md:mb-12 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="64-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇩🇪"
+              data-translation="🇩🇪" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇩🇪</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="64-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇫🇷"
+              data-translation="🇫🇷" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇫🇷</ya-tr-span></span></button>
+        </div>
+      </div>
+      <div
+          class="mb-10 text-center lg:mb-10 py-12 md:py-52 bg-[url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/gali/worldmap.png')] bg-contain bg-center bg-no-repeat">
+        <button disabled=""
+                class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg text-sm border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 mb-4"
+                type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><svg
+            width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path
+            d="M22.4 12.5L22.175 12.8L22.4 13.1L26.48 18.54C26.6026 18.7035 26.6773 18.8978 26.6956 19.1013C26.714 19.3047 26.6753 19.5093 26.5839 19.692C26.4926 19.8747 26.3521 20.0284 26.1784 20.1358C26.0046 20.2432 25.8043 20.3 25.6 20.3H9.60005C9.04309 20.3 8.50895 20.5213 8.11512 20.9151C7.7213 21.309 7.50005 21.8431 7.50005 22.4V27.2C7.50005 27.4918 7.38416 27.7716 7.17787 27.9779C6.97158 28.1842 6.69179 28.3 6.40005 28.3C6.10831 28.3 5.82852 28.1842 5.62223 27.9779C5.41594 27.7716 5.30005 27.4918 5.30005 27.2V9.60005C5.30005 8.45962 5.75308 7.3659 6.55949 6.55949C7.3659 5.75308 8.45962 5.30005 9.60005 5.30005H25.6C25.8043 5.30005 26.0046 5.35694 26.1784 5.46433C26.3521 5.57173 26.4926 5.7254 26.5839 5.90811C26.6753 6.09083 26.714 6.29538 26.6956 6.49884C26.6773 6.70229 26.6026 6.89662 26.48 7.06005L22.4 12.5Z"
+            fill="#212121" stroke="#212121"></path></svg></span></button>
+        <h2 class="block antialiased tracking-normal font-sans text-4xl leading-[1.3] text-blue-gray-900 mb-4 font-extrabold">
+          <ya-tr-span data-index="65-0" data-translated="false" data-source-lang="en" data-target-lang="ru"
+                      data-value="50+ Languages" data-translation="Более 50 языков" data-ch="0" data-type="trSpan"
+                      style="visibility: initial !important;">50+ языков
+          </ya-tr-span>
+        </h2>
+        <p class="block antialiased font-sans text-base leading-relaxed text-blue-gray-900 mx-auto font-bold mt-2 !text-xl max-w-2xl">
+          <ya-tr-span data-index="66-0" data-translated="false" data-source-lang="en" data-target-lang="ru"
+                      data-value="Whether your customers are from USA, Spain, Germany, France or anywhere, our chatbot speaks their language."
+                      data-translation="Независимо от того, из США, Испании, Германии, Франции или откуда угодно еще ваши клиенты, наш чат-бот говорит на их языке."
+                      data-ch="0" data-type="trSpan" data-selected="false" style="visibility: initial !important;">
+            В панели управлении сайтом вы сможете добавлять
+            различные страны, города и районы.
+          </ya-tr-span>
+        </p>
+      </div>
+      <div class="flex items-center justify-center">
+        <div class="flex md:flex-col items-center md:mr-8">
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl md:mb-12"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="67-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇪🇸"
+              data-translation="🇪🇸" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇪🇸</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl md:mb-12"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="67-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇷🇺"
+              data-translation="🇷🇺" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇷🇺</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="67-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇮🇹"
+              data-translation="🇮🇹" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇮🇹</ya-tr-span></span></button>
+        </div>
+        <div class="flex md:flex-col items-center">
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl md:mb-12"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="68-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇧🇷"
+              data-translation="🇧🇷" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇧🇷</ya-tr-span></span></button>
+          <button disabled=""
+                  class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] rounded-lg border text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] p-6 !opacity-100 border-gray-300 text-2xl"
+                  type="button"><span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><ya-tr-span
+              data-index="68-0" data-translated="false" data-source-lang="en" data-target-lang="ru" data-value="🇯🇵"
+              data-translation="🇯🇵" data-ch="0" data-type="trSpan"
+              style="visibility: initial !important;">🇯🇵</ya-tr-span></span></button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
