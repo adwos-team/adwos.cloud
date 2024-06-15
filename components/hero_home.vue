608211cee6ff6533baafe8e0e6f315b02cdff104
adwos <makar.savchuk.2020@gmail.com>
2024-06-15 05:08:35 +0300

    add update

--- components/hero_home.vue
+++ components/hero_home.vue
@@ -1,21 +1,4 @@
 <template>
-  <!--  <div class="relative  min-h-full w-full bg-[url('/images/header-bg-blob2.jpg')] bg-top bg-cover bg-no-repeat ">-->
-  <!--    <div class="grid min-h-screen px-8">-->
-  <!--      <div class="container relative z-10 my-auto mx-auto text-center">-->
-  <!--        <div-->
-  <!--            class="relative items-center font-sans font-bold whitespace-nowrap select-none py-1.5 px-3 text-xs capitalize inline-flex rounded-full bg-gray-900 text-gray-100 mb-4"-->
-  <!--            style="opacity: 1;"><span class="">Дорожная карта проекта</span></div>-->
-  <!--        <h1 class="block antialiased tracking-normal font-sans leading-tight text-gray-900 font-extrabold text-2xl md:text-3xl lg:text-5xl">-->
-  <!--          Adwos CMS Профессиональная система <br> управления доской обьявлений</h1>-->
-  <!--        <p class="block antialiased font-sans font-normal leading-relaxed mt-2 md:px-32 lg:px-52 text-gray-700 text-lg mt-2 lg:text-xl">-->
-  <!--          Готовая система для запуска досок объявлений, маркетплейсов, сервисов аренды и бронирования. Со своим мобильным приложением для Android и iOS</p></div>-->
-  <!--      <div class="mt-18 relative">-->
-  <!--        <img-->
-  <!--          class="absolute z-10 rounded-xl -translate-x-1/2 left-1/2 -mt-12 w-full max-w-[740px] lg:w-1/2 shadow-2xl"-->
-  <!--          src="/images/svg/home_board.svg">-->
-  <!--        </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
 
   <header class="h-full w-screen place-items-center bg-white relative px-8 py-8 lg:mb-36">
     <div class="container mx-auto grid items-center lg:grid-cols-2">
